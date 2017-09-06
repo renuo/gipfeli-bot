e823bd581560cdeb9db250182f85fb8e2ca0e4ed
Denis Mueller <denis.mueller@renuo.ch>
2017-09-06 16:35:44 +0200

    try memcache

--- slack-gipfeli-bot/commands/add.rb
+++ slack-gipfeli-bot/commands/add.rb
@@ -1,6 +1,15 @@
 module SlackGipfeliBot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
+			require 'dalli'
+			cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+													{:username => ENV["MEMCACHIER_USERNAME"],
+													 :password => ENV["MEMCACHIER_PASSWORD"],
+													 :failover => true,
+													 :socket_timeout => 1.5,
+													 :socket_failure_delay => 0.2
+													})
+
       command 'add' do |client, data, _match|
         list = File.open('gipfeli_list', 'a')
 
@@ -9,7 +18,9 @@ module SlackGipfeliBot
           client.say(channel: data.channel, text: 'You need to tell me your order! (e.g. \'add gipfeli\'')
         else
           client.say(channel: data.channel, text: "Your order of '#{wish}' has been added to the list.")
-          list.write("#{wish}\n")
+					cache.set('list', "#{cache.get('list')} \n #{wish}")	
+					client.say(channel: data.channel, text: cache.get('list'))
+          #list.write("#{wish}\n")
         end
 
         list.close
