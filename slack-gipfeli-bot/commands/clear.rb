9278c5b9a8d11c66686feef5676c41418e5f7dab
Denis Mueller <denis.mueller@renuo.ch>
2017-09-06 16:50:10 +0200

    fix another typo

--- slack-gipfeli-bot/commands/clear.rb
+++ slack-gipfeli-bot/commands/clear.rb
@@ -1,6 +1,15 @@
 module SlackGipfeliBot
   module Commands
     class Clear< SlackRubyBot::Commands::Base
+
+			require 'dalli'
+			cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+													{:username => ENV["MEMCACHIER_USERNAME"],
+													 :password => ENV["MEMCACHIER_PASSWORD"],
+													 :failover => true,
+													 :socket_timeout => 1.5,
+													 :socket_failure_delay => 0.2
+													})
       command 'clear' do |client, data, _match|
 				if cache.get('list') != nil 
 					cache.set('list', nil) 
--- slack-gipfeli-bot/commands/show.rb
+++ slack-gipfeli-bot/commands/show.rb
@@ -2,6 +2,14 @@ module SlackGipfeliBot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
 
+			require 'dalli'
+			cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+													{:username => ENV["MEMCACHIER_USERNAME"],
+													 :password => ENV["MEMCACHIER_PASSWORD"],
+													 :failover => true,
+													 :socket_timeout => 1.5,
+													 :socket_failure_delay => 0.2
+													})
       command 'show' do |client, data, _match|
 					if cache.get('list') == nil
 						text = 'There are no orders currently'	
