ebe570b98f070acf1d7c6036ed6a9e3eebe97dba
Denis Mueller <denis.mueller@renuo.ch>
2017-09-07 09:45:00 +0200

    add crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,16 @@
 
 # Learn more: http://github.com/javan/whenever
 #
-every 1.day, :at => '14:41' do
-	File.delete('gipfeli_list')
+
+			require 'dalli'
+			cache = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
+													{:username => ENV["MEMCACHIER_USERNAME"],
+													 :password => ENV["MEMCACHIER_PASSWORD"],
+													 :failover => true,
+													 :socket_timeout => 1.5,
+													 :socket_failure_delay => 0.2
+													})
+every 1.day, :at => '09:50' do
+	cache.set('list', nil)
 end
 
