d626be9c494af7b45f2c11647c39b16d8ad1ec57
Sebastian Rothmund <sebastian.rothmund@renuo.ch>
2017-09-08 14:52:56 +0200

    Formatting

--- slack-gipfeli-bot/commands/help.rb
+++ slack-gipfeli-bot/commands/help.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 module SlackGipfeliBot
   module Commands
-    class Help< SlackRubyBot::Commands::Base
+    class Help < SlackRubyBot::Commands::Base
       command 'help' do |client, data, _match|
-				text = " 
+        text = "
 					add [Item name] --> add a new item to the list \n
 					show --> show all items on current list \n
-					clear --> empty the list	
-				" 
+					clear --> empty the list
+				"
         client.say(channel: data.channel, text: text)
       end
     end
