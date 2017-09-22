fc2c3c52e11d7f815e06d623e58fac8d64f0c066
Sebastian Rothmund <sebastian.rothmund@renuo.ch>
2017-09-22 14:24:37 +0200

    Rubocop

--- slack-gipfeli-bot/commands/remind.rb
+++ slack-gipfeli-bot/commands/remind.rb
@@ -8,8 +8,9 @@ module SlackGipfeliBot
           client.say(channel: ENV['SLACK_FOOD_CHANNEL_ID'], text:
               "*Someone is about to go buy Gipfeli!*\n"\
               '*Place your orders in the next ~5 minutes or it\'s too late!*')
-          sleep(5*60)
-          client.say(channel: data.channel, text: '*You waited long enough, you can go buy them now! (Don\'t forget to clear!)*')
+          sleep(5 * 60)
+          client.say(channel: data.channel, text:
+              '*You waited long enough, you can go buy them now! (Don\'t forget to clear!)*')
         end
       end
     end
